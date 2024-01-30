3abd670faf51e832c95d52f1f2bd275514799a25
HarryMeng <harry.mld.2010@gmail.com>
2024-01-29 21:34:56 -0800

    Update sign up page with form Update resgister page with form

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 const Register = () => {
-  return <div>here is to register restaurant</div>;
+  return (
+    <div className="col-lg-6 col-md-8 col-sm-12 mx-auto bg-white shadow p-4">
+      <h2 className="text-center mb-3">Register a Restaurant</h2>
+      <Form>
+        <Row>
+          <Form.Group as={Col}>
+            <Form.Label>Company Name</Form.Label>
+            <Form.Control placeholder="Company Name" />
+          </Form.Group>
+
+          <Form.Group as={Col}>
+            <Form.Label>Company Email</Form.Label>
+            <Form.Control placeholder="Company Email" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group as={Col}>
+          <Form.Label>Address</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
+        </Form.Group>
+
+        <Row>
+          <Form.Group as={Col}>
+            <Form.Label>City</Form.Label>
+            <Form.Control placeholder="" />
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Label>State</Form.Label>
+            <Form.Control placeholder="" />
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Label>Zip Code</Form.Label>
+            <Form.Control placeholder="" />
+          </Form.Group>
+        </Row>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Register;
--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 const SignUp = () => {
-  return <div>here is to sign up as a new user</div>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const updateFirstName = () => {
+    setFirstName(event.target.value);
+  };
+
+  const updateLastName = () => {
+    setLastName(event.target.value);
+  };
+
+  const updatePassword = () => {
+    setPassword(event.target.value);
+  };
+
+  const updateConfirmPassword = () => {
+    setConfirmPassword(event.target.value);
+  };
+
+  return (
+
+    <div className="col-lg-6 col-md-8 col-sm-12 mx-auto bg-white shadow p-4">
+      <h2 className="text-center mb-3">Create an Account</h2>
+      <Form>
+        <Row>
+          <Form.Group as={Col}>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control placeholder="First name" />
+          </Form.Group>
+
+          <Form.Group as={Col}>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control placeholder="Last name" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Row>
+          <Form.Group as={Col}>
+            <Form.Label>password</Form.Label>
+            <Form.Control placeholder="Password" />
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control placeholder="Confirm Password" />
+          </Form.Group>
+        </Row>
+        <Button variant="primary" type="submit">
+          Create
+        </Button>
+      </Form>
+
+      <div>Already have an account? Sign In Here</div>
+    </div>
+  );
 };
 
 export default SignUp;
